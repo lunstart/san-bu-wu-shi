42ee50e408876ba01c33271f7241cfd779b6d5d3
ljwokk <ljw4328.gh@gmail.com>
2024-02-05 16:53:55 +0800

    test01

--- san-pojo/src/main/java/com/sky/entity/Employee.java
+++ san-pojo/src/main/java/com/sky/entity/Employee.java
@@ -32,14 +32,14 @@ public class Employee implements Serializable {
 
     private Integer status;
 
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime createTime;
-
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime updateTime;
-
-    private Long createUser;
-
-    private Long updateUser;
+//    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    private LocalDateTime createTime;
+//
+//    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//    private LocalDateTime updateTime;
+//
+//    private Long createUser;
+//
+//    private Long updateUser;
 
 }
